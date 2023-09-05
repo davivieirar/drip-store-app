7b141a225494e088b199e99b058bd546d844f486
LuccasVieira <118361303+LuccasVieira@users.noreply.github.com>
2023-09-04 21:56:10 -0300

    Iniciando Home

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
+import { TableDefault } from "./components/TableDefault";
+import { Home } from "./pages/home/Home";
+
+
+function App() {
+  return (
+    <>
+      <Home/>
+    </>
+  );
+}
+
+export default App;
+
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { MenuBar } from '../../components/header/MenuBar'
+
+export const Home = () => {
+    return (
+        <MenuBar />
+        
+    )
+}
+
